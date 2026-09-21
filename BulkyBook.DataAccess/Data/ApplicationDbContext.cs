7773bff79511a53a002e1197ff3b569812ef54fc
FAETHON THEODOROS ANTONOPOULOS <antonopfaeth@gmail.com>
2026-09-21 10:05:06 +0300

    added context and migration for product table

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ public class ApplicationDbContext : DbContext
     {}
     
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
     
     protected override void OnModelCreating(ModelBuilder modelBuilder){
         //base.OnModelCreating(modelBuilder);
--- BulkyBook.Models/Product.cs
+++ BulkyBook.Models/Product.cs
@@ -1,6 +1,42 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BulkyBook.Models;
 
 public class Product
 {
+    public int Id { get; set; }
     
+    [Required]
+    public string Title { get; set; } = string.Empty;
+    
+    public string Description { get; set; } = string.Empty;
+    
+    [Required]
+    public string ISBN { get; set; } = string.Empty;
+    
+    [Required]
+    public string Author { get; set; } = string.Empty;
+    
+    [Required]
+    [Display(Name = "List Price")]
+    [Range(0, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
+    public double ListPrice { get; set; }
+    
+    [Required]
+    [Display(Name = "Price for 1-50")]
+    [Range(0, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
+    public double Price { get; set; }
+    
+    [Required]
+    [Display(Name = "Price for 50+")]
+    [Range(0, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
+    public double Price50 { get; set; }
+    
+    [Required]
+    [Display(Name = "Price for 100+")]
+    [Range(0, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
+    public double Price100 { get; set; }
+    
+    [Display(Name = "Product Image")]
+    public string? ImageUrl { get; set; }
 }
